9a4ffddf1fa9d1e525d8459300271a3451f1202d
AJ Ancheta <7781450+ancheetah@users.noreply.github.com>
2025-02-15 10:23:37 -0800

    render first step

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import './style.css'
-import { Config, FRAuth } from '@forgerock/javascript-sdk';
+import { Config, FRAuth, FRCallback, FRStep } from '@forgerock/javascript-sdk';
 
 Config.set({
   clientId: 'aj-public-sdk-client', // e.g. 'ForgeRockSDKClient'
@@ -13,5 +13,63 @@ Config.set({
   tree: 'Login', // e.g. 'sdkAuthenticationTree' or 'Login'
 });
 
-const firstStep = FRAuth.start();
-console.log('firstStep', firstStep);
+const panelFormElem = document.getElementById('panel-form');
+const submitButtonElem = document.getElementById('submit-button');
+
+async function handleSubmit(event: Event, step?: FRStep) {
+  event.preventDefault();
+  console.log('submitted!', step);
+}
+
+async function nextStep(previousStep?: FRStep) {
+  try {
+    const nextStep = await FRAuth.next(previousStep);
+    console.log('nextStep', nextStep);
+    if (nextStep.type === 'Step') {
+      nextStep.callbacks.forEach(mapCallbacksToComponents);
+    } else {
+      // Handle login success or failure
+    }
+  } catch (error) {
+    console.error('nextStep error: ', error);
+    if (panelFormElem) {
+      panelFormElem.innerHTML = 'Error';
+    } 
+  }
+} 
+
+
+function renderTextInputCallback(cb: FRCallback, inputID: string) {
+  const prompt = cb.getOutputByName('prompt', '');
+  const wrapper = document.createElement('div');
+
+  const label = document.createElement('label');
+  label.setAttribute('for', prompt);
+  label.innerHTML = prompt;
+
+  const input = document.createElement('input');
+  input.setAttribute('type', cb.getType() === 'PasswordCallback' ? 'password' : 'text');
+  input.setAttribute('id', inputID);
+  input.setAttribute('name', inputID);
+  input.setAttribute('required', 'true');
+
+  wrapper.appendChild(label);
+  wrapper.appendChild(input);
+
+  panelFormElem?.insertBefore(wrapper, submitButtonElem);
+}
+
+function mapCallbacksToComponents(cb: FRCallback) {
+  const cbType = cb.getType();
+  switch (cbType) {
+    case 'NameCallback':
+    case 'PasswordCallback':
+      renderTextInputCallback(cb, cbType === 'NameCallback' ? 'name' : 'password');
+      break;
+    default:
+      throw new Error(`Unknown callback type: ${cbType}`);
+  }
+}
+
+await nextStep();
+panelFormElem?.addEventListener('submit', (event) => handleSubmit(event));
