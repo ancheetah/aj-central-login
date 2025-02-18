0e85345e79c47f414156f2d427b2f24bac47bda5
AJ Ancheta <7781450+ancheetah@users.noreply.github.com>
2025-02-18 08:38:58 -0800

    set name/password callbacks

--- src/main.ts
+++ src/main.ts
@@ -1,57 +1,82 @@
-import './style.css'
-import { Config, FRAuth, FRCallback, FRStep } from '@forgerock/javascript-sdk';
+import "./style.css";
+import { Config, FRAuth, FRCallback, FRStep } from "@forgerock/javascript-sdk";
 
 Config.set({
-  clientId: 'aj-public-sdk-client', // e.g. 'ForgeRockSDKClient'
+  clientId: "aj-public-sdk-client", // e.g. 'ForgeRockSDKClient'
   redirectUri: `http://localhost:3000`, // e.g. 'https://sdkapp.example.com:8443/callback.html'
-  scope: 'openid profile email address revoke', // e.g. 'openid profile email address phone'
+  scope: "openid profile email address revoke", // e.g. 'openid profile email address phone'
   serverConfig: {
-      baseUrl: 'https://openam-sdks.forgeblocks.com/am', // e.g. 'https://myorg.forgeblocks.com/am' or 'https://openam.example.com:8443/openam'
-      timeout: 3000, // 90000 or less
+    baseUrl: "https://openam-sdks.forgeblocks.com/am", // e.g. 'https://myorg.forgeblocks.com/am' or 'https://openam.example.com:8443/openam'
+    timeout: 3000, // 90000 or less
   },
-  realmPath: 'alpha', // e.g. 'alpha' or 'root'
-  tree: 'Login', // e.g. 'sdkAuthenticationTree' or 'Login'
+  realmPath: "alpha", // e.g. 'alpha' or 'root'
+  tree: "Login", // e.g. 'sdkAuthenticationTree' or 'Login'
 });
 
-const panelFormElem = document.getElementById('panel-form');
-const submitButtonElem = document.getElementById('submit-button');
+const panelFormElem = document.getElementById("panel-form");
+const submitButtonElem = document.getElementById("submit-button");
+
+function setCallbackValue(cb: FRCallback): void {
+  const cbType = cb.getType();
+  if (["NameCallback", "PasswordCallback"].includes(cbType)) {
+    const input = panelFormElem?.querySelector(
+      `input[name=${cbType === "NameCallback" ? "username" : "password"}]`
+    );
+    if (input instanceof HTMLInputElement) {
+      cb.setInputValue(input.value);
+    } else {
+      throw new Error(`Failed to set callback. Not found. ${cbType}`);
+    }
+  } else {
+    throw new Error(`Failed to set callback. Unknown callback type: ${cbType}`);
+  }
+}
 
 async function handleSubmit(event: Event, step?: FRStep): Promise<void> {
   event.preventDefault();
-  console.log('submitted!', step);
+  step?.callbacks.forEach((cb) => setCallbackValue(cb));
+  await nextStep(step);
+  console.log("submitted!", step);
 }
 
 async function nextStep(previousStep?: FRStep): Promise<void> {
   try {
     const nextStep = await FRAuth.next(previousStep);
-    console.log('nextStep', nextStep);
-    if (nextStep.type === 'Step') {
+    console.log("nextStep", nextStep);
+    if (nextStep.type === "Step") {
       nextStep.callbacks.forEach(mapCallbacksToComponents);
+      panelFormElem?.removeEventListener("submit", handleSubmit);
+      panelFormElem?.addEventListener("submit", (event) =>
+        handleSubmit(event, nextStep)
+      );
     } else {
       // Handle login success or failure
+      console.log("todo: handle login success or failure");
     }
   } catch (error) {
-    console.error('nextStep error: ', error);
+    console.error("nextStep error: ", error);
     if (panelFormElem) {
-      panelFormElem.innerHTML = 'Error';
-    } 
+      panelFormElem.innerHTML = "Error";
+    }
   }
-} 
-
+}
 
 function renderTextInputCallback(cb: FRCallback, inputID: string): void {
-  const prompt = cb.getOutputByName('prompt', '');
-  const wrapper = document.createElement('div');
+  const prompt = cb.getOutputByName("prompt", "");
+  const wrapper = document.createElement("div");
 
-  const label = document.createElement('label');
-  label.setAttribute('for', prompt);
+  const label = document.createElement("label");
+  label.setAttribute("for", prompt);
   label.innerHTML = prompt;
 
-  const input = document.createElement('input');
-  input.setAttribute('type', cb.getType() === 'PasswordCallback' ? 'password' : 'text');
-  input.setAttribute('id', inputID);
-  input.setAttribute('name', inputID);
-  input.setAttribute('required', 'true');
+  const input = document.createElement("input");
+  input.setAttribute(
+    "type",
+    cb.getType() === "PasswordCallback" ? "password" : "text"
+  );
+  // input.setAttribute("id", inputID);
+  input.setAttribute("name", inputID);
+  input.setAttribute("required", "true");
 
   wrapper.appendChild(label);
   wrapper.appendChild(input);
@@ -62,9 +87,12 @@ function renderTextInputCallback(cb: FRCallback, inputID: string): void {
 function mapCallbacksToComponents(cb: FRCallback): void {
   const cbType = cb.getType();
   switch (cbType) {
-    case 'NameCallback':
-    case 'PasswordCallback':
-      renderTextInputCallback(cb, cbType === 'NameCallback' ? 'name' : 'password');
+    case "NameCallback":
+    case "PasswordCallback":
+      renderTextInputCallback(
+        cb,
+        cbType === "NameCallback" ? "username" : "password"
+      );
       break;
     default:
       throw new Error(`Unknown callback type: ${cbType}`);
@@ -72,4 +100,3 @@ function mapCallbacksToComponents(cb: FRCallback): void {
 }
 
 await nextStep();
-panelFormElem?.addEventListener('submit', (event) => handleSubmit(event));
