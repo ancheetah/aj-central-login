7ba4c297cc7b31e8fe8f84133ba0c64f9fbf76e9
AJ Ancheta <7781450+ancheetah@users.noreply.github.com>
2025-02-19 10:51:47 -0800

    handle force renew

--- src/main.ts
+++ src/main.ts
@@ -18,14 +18,39 @@ Config.set({
   // tree: "AJ Choice Login", // e.g. 'sdkAuthenticationTree' or 'Login'
 });
 
-async function handleLogin(): Promise<void> {
+interface LoginOptions {
+  forceRenew?: boolean;
+}
+
+const userElem = document.getElementById("user");
+
+async function handleLogin(
+  _event: Event,
+  options?: LoginOptions
+): Promise<void> {
+  const { forceRenew } = options ?? {};
   try {
     // Start authenitcation flow
     // 'redirect' option enables central login directly with auth server
-    await TokenManager.getTokens({
+    hideUser();
+    const tokens = await TokenManager.getTokens({
       login: "redirect",
       query: { acr_values: "aj-choice" },
+      forceRenew,
     });
+
+    // Show user if force renewed tokens with no redirect
+    if (tokens) {
+      const { accessToken } = tokens;
+      console.log("accessToken", accessToken);
+
+      if (!accessToken) {
+        throw new Error("Failed to get access token");
+      }
+
+      const user = await UserManager.getCurrentUser();
+      showUser(user);
+    }
   } catch (err) {
     console.error("Failed to start authentication flow", err);
   }
@@ -33,6 +58,7 @@ async function handleLogin(): Promise<void> {
 
 async function handleLogout(): Promise<void> {
   try {
+    hideUser();
     await FRUser.logout();
     location.assign(window.location.origin);
   } catch (err) {
@@ -43,32 +69,45 @@ async function handleLogout(): Promise<void> {
 async function authorize(code: string, state: string): Promise<void> {
   console.log("authorizing...", code, state);
 
-  // Trade code and state for an access token
-  const tokens = await TokenManager.getTokens({ query: { code, state } });
-  if (!tokens) {
-    throw new Error("Failed to get tokens");
-  }
-  const { accessToken } = tokens;
-  console.log("accessToken", accessToken);
+  try {
+    // Trade code and state for an access token
+    const tokens = await TokenManager.getTokens({ query: { code, state } });
+    if (!tokens) {
+      throw new Error("Failed to get tokens");
+    }
 
-  if (!accessToken) {
-    throw new Error("Failed to get access token");
-  }
+    const { accessToken } = tokens;
+    console.log("accessToken", accessToken);
 
-  const user = await UserManager.getCurrentUser();
-  showUser(user);
+    if (!accessToken) {
+      throw new Error("Failed to get access token");
+    }
+
+    const user = await UserManager.getCurrentUser();
+    showUser(user);
+  } catch (err) {
+    console.error("Failed to authorize", err);
+  }
 }
 
 function showUser(user: unknown): void {
-  const userElem = document.getElementById("user");
   if (userElem) {
     userElem.innerHTML = JSON.stringify(user, null, 2);
     userElem.style.display = "block";
   }
 }
 
+function hideUser(): void {
+  if (userElem) {
+    userElem.style.display = "none";
+  }
+}
+
 // Add login/logout event listeners
 document.getElementById("login-button")?.addEventListener("click", handleLogin);
+document
+  .getElementById("force-renew-button")
+  ?.addEventListener("click", (e) => handleLogin(e, { forceRenew: true }));
 document
   .getElementById("logout-button")
   ?.addEventListener("click", handleLogout);
